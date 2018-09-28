22f39a89e8c711c5bea46e01910d6a1e89b25e08
Raul Aguilar <raula@live.com>
2018-09-27 22:51:23 -0700

    Update Homework 5
    added project files to homework 5. Outline for fill project

---
+++ Homework 5/fill/Fill.asm
@@ -0,0 +1,29 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+
+// while @KBD doesn't equal 0 peform while-loop
+
+
+// paints first screen pixel black
+@SCREEN     // set A to 16384
+M=-1        // set first pixel memory address to -1 (black)
+
+// make a while loop to increment screen address while key is held
+(LOOP)
+
+// infinite loop to end program
+(InfiniteLoop)
+@InfiniteLoop
+0; JMP
